90a97cce0c867b38b2fd880c6be33b2805073000
Yaqub Ja'e <83901258+Yaqub-J@users.noreply.github.com>
2025-05-20 13:31:23 +0000

    Restored to '2b920c7d13fa9089b94043138b717ee50b2513bf'
    Replit-Restored-To: 2b920c7d13fa9089b94043138b717ee50b2513bf

--- next.config.js
+++ next.config.js
@@ -1,30 +1,14 @@
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-  images: {
-    domains: ['images.unsplash.com'],
-  },
-  async headers() {
-    return [
-      {
-        source: '/:path*',
-        headers: [
-          {
-            key: 'Content-Security-Policy',
-            value: "script-src 'self' 'unsafe-inline' 'unsafe-eval' https://js.hydrogen.co;"
-          }
+    images: {
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'images.pexels.com',
+            },
         ],
-      },
-    ]
-  },
-  scripts: [
-    {
-      src: "https://js.hydrogen.co/v1/inline.js",
-      strategy: "beforeInteractive"
-    }
-  ]
+    },
 }
 
 module.exports = nextConfig
